9f19e69235e837801bac5196707a9559ed24772a
Voleco <chenjingwei1991@gmail.com>
2016-05-24 17:05:15 -0600

    add sfbds

---
+++ algorithms/MySFBDS.h
@@ -0,0 +1,178 @@
+//my implementation of single frontier bidirectional search
+#ifndef MY_SFBDS_H
+#define MY_SFBDS_H
+
+#include <vector>
+#include <stack>
+#include "MySearchAlgorithm.h"
+
+const double DBL_ERROR = 0.0001;
+
+inline bool fless(double a, double b) { return (a < b - DBL_ERROR); }
+inline bool fgreater(double a, double b) { return (a > b + DBL_ERROR); }
+inline bool fequal(double a, double b){	return (a >= b - DBL_ERROR) && (a <= b + DBL_ERROR);}
+
+enum JumpingPolicy
+{
+	JIL0 = 0,
+	JIL1 = 1,
+	JIL2 = 2,
+	BF = 3
+};
+
+template <typename state, typename action, typename environment, typename heuristic>
+class MySFBDS :public MySearchAlgorithm<state, action, environment>
+{
+public:
+	MySFBDS(heuristic& fw, heuristic& bw, JumpingPolicy jp, double maxF=100000)
+		:MySearchAlgorithm<state, action, environment>(), nodesExpanded(0),numJumps(0),
+		forwardHeur(fw), backwardHeur(bw),maxFAllowed(maxF),nextF(-1),policy(jp)
+	{
+		std::stack<action> sqs;
+		actionSequences.push_back(sqs);
+		actionSequences.push_back(sqs);
+	}
+	virtual bool GetPath(environment& e, state& start, state& goal);
+	bool GetPathWithinF(environment& e, state& start, state& goal, double f, double gcost, int direction);
+	virtual uint64_t GetNodesExpanded() { return nodesExpanded; }
+	uint64_t GetNumOfJumps() { return numJumps; }
+	std::vector<action> GetActionSequence();
+protected:
+	uint64_t nodesExpanded;
+	uint64_t numJumps;
+	heuristic forwardHeur;
+	heuristic backwardHeur;
+
+	std::vector<std::stack<action>> actionSequences;
+
+	JumpingPolicy policy;
+	double nextF;
+	double maxFAllowed;
+};
+
+
+template <typename state, typename action, typename environment, typename heuristic>
+bool MySFBDS<state, action, environment, heuristic>::GetPath(environment& e, state& start, state& goal)
+{
+	//while(!actionSequence.empty())
+	//	actionSequence.pop();
+	nextF = -1;
+	while (!actionSequences[0].empty())
+		actionSequences[0].pop();
+	while (!actionSequences[1].empty())
+		actionSequences[1].pop();
+	nodesExpanded = 0;
+	numJumps = 0;
+	while (nextF< maxFAllowed)
+	{
+		//std::cout << "nextF" << nextF << "\n";
+		if (GetPathWithinF(e, start, goal, nextF, 0, 0))
+			return true;
+	}
+	return false;
+}
+
+template <typename state, typename action, typename environment, typename heuristic>
+bool MySFBDS<state, action, environment, heuristic>::GetPathWithinF(environment& e, state& start, state& goal, double f,double gcost, int direction)
+{
+	double fwhcost = forwardHeur.GetHCost(start,goal);
+	double bwhcost = backwardHeur.GetHCost(goal, start);
+	
+	double fcost = gcost + (fless(fwhcost, bwhcost) ? fwhcost : bwhcost);
+	//double fcost = gcost + fwhcost;
+	//std::cout << "fcost: " << fcost << "\n";
+	if (fgreater(fcost, f))
+	//if (fcost > f)
+	{
+		if (fequal(nextF,f))
+		//if (nextF== f)
+			nextF = fcost;
+		else if (fgreater(nextF,f) && fless(fcost, nextF))
+		//else if (nextF> f && fcost< nextF)
+			nextF = fcost;
+
+		return false;
+	}
+	nodesExpanded++;
+	if (start == goal)
+		return true;
+	//determine the direction to expand
+	int dir = 0;
+	std::vector<action> actions;
+	int fbf;
+	int bbf;
+	switch (policy)
+	{
+	case JIL0:
+		break;
+	case JIL1:
+		break;
+	case JIL2:
+		break;
+	case BF:
+		e.GetActions(start, actions);
+		fbf = actions.size();
+		e.GetActions(goal, actions);
+		bbf = actions.size();
+		if (bbf < fbf)
+			dir = 1;
+		break;
+	default:
+		break;
+	}
+	if (dir != direction)
+		numJumps++;
+	
+	if(dir==0)
+		e.GetActions(start, actions);
+	else//dir == 1
+		e.GetActions(goal, actions);
+	for (int i = 0; i < actions.size(); i++)
+	{
+		if (!actionSequences[dir].empty() && 
+			e.GetInvertAction(actions[i]) == actionSequences[dir].top())
+			continue;
+		if(dir==0)
+			e.ApplyAction(start, actions[i]);
+		else
+			e.ApplyAction(goal, actions[i]);
+		actionSequences[dir].push(actions[i]);
+		
+
+		if (GetPathWithinF(e, start, goal, f, gcost + e.GetActionCost(actions[i]),dir))
+			return true;
+
+		if (dir == 0)
+			e.UndoAction(start, actions[i]);
+		else
+			e.UndoAction(goal, actions[i]);
+		actionSequences[dir].pop();
+	}
+
+	return false;
+}
+
+template <typename state, typename action, typename environment, typename heuristic>
+std::vector<action> MySFBDS<state, action, environment, heuristic>::GetActionSequence()
+{
+	std::vector<action> result;
+	std::stack<action> tmp;
+	//use 2 stacks as 1 queue
+	while (!actionSequences[0].empty())
+	{
+		tmp.push(actionSequences[0].top());
+		actionSequences[0].pop();
+	}
+	while (!tmp.empty())
+	{
+		result.push_back(tmp.top());
+		tmp.pop();
+	}
+	while (!actionSequences[1].empty())
+	{
+		result.push_back(actionSequences[1].top());
+		actionSequences[1].pop();
+	}
+	return result;
+}
+#endif // ! MY_SFBDS_H
---
+++ apps/sfbdstest/Driver.cpp
@@ -0,0 +1,495 @@
+
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <cstring>
+#include "MyIDAStar.h"
+#include "MyAStar.h"
+#include "SlidingTilePuzzle.h"
+#include "Map2D.h"
+#include "MySFBDS.h"
+
+
+bool LoadBenchmark(std::vector<int>& startx, std::vector<int>& starty, std::vector<int>& goalx, std::vector<int>& goaly,
+	std::vector<double>& expectedCost, std::string fileName);
+
+int main(int argc,char** argv)
+{
+	if (argc > 1 && strcmp(argv[1], "-stp") == 0)
+	{
+		bool showPath = false;
+		int low = 1;
+		int high = 100;
+		if (argc > 3)
+		{
+			low = atoi(argv[2]);
+			high = atoi(argv[3]);
+		}
+		if (argc > 4 && strcmp(argv[4], "-showpath") == 0)
+			showPath = true;
+
+		SlidingTilePuzzle puzzle(4, 4);
+		SlidingTilePuzzleState start(4, 4);
+		SlidingTilePuzzleState goal(4, 4);
+		ManhattanDistanceHeuristic heur;
+		heur.SetGoal(goal);
+
+
+		MyIDAStar<SlidingTilePuzzleState, SlidingTilePuzzleAction, SlidingTilePuzzle, ManhattanDistanceHeuristic>
+			*ida;
+
+		clock_t startTime;
+		clock_t endTime;
+		clock_t clockTicksTaken;
+		double timeInSeconds;
+		//int i = atoi(argv[1]);
+		for (int i = low - 1; i < high; i++)
+		{
+			startTime = clock();
+			GetSildingTileInstance(i, start);
+			ida = new MyIDAStar<SlidingTilePuzzleState, SlidingTilePuzzleAction, SlidingTilePuzzle, ManhattanDistanceHeuristic>(heur, 500);
+			std::cout << "********************************\n"
+				<< "Puzzle " << i + 1 << " of 100\n"
+				<< "start: " << start
+				<< "goal: " << goal << "\n";
+			std::vector<SlidingTilePuzzleAction> acs;
+			puzzle = SlidingTilePuzzle(4, 4);
+			if (ida->GetPath(puzzle, start, goal))
+			{
+				std::cout << "IDA* found a path!\n";
+				acs = ida->GetActionSequence();
+				std::cout << "Path length: " << acs.size() << "\n";
+				if (showPath)
+				{
+					std::cout << "Path: ";
+					for (int i = 0; i < acs.size(); i++)
+						std::cout << acs[i] << " ";
+					std::cout << "\n";
+				}
+			}
+
+			else
+				std::cout << "IDA* did not find a path.\n";
+			std::cout << "nodes expanded: " << ida->GetNodesExpanded() << "\n";
+
+			delete ida;
+
+			endTime = clock();
+			clockTicksTaken = endTime - startTime;
+			timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;
+			std::cout << "time spent: " << timeInSeconds << " seconds\n";
+		}
+
+	}
+
+	else if (argc > 1 && strcmp(argv[1], "-bf") == 0)
+	{
+		bool showPath = false;
+		int low = 1;
+		int high = 100;
+		if (argc > 3)
+		{
+			low = atoi(argv[2]);
+			high = atoi(argv[3]);
+		}
+		if (argc > 4 && strcmp(argv[4], "-showpath") == 0)
+			showPath = true;
+
+		SlidingTilePuzzle puzzle(4, 4);
+		SlidingTilePuzzleState start(4, 4);
+		SlidingTilePuzzleState goal(4, 4);
+		ManhattanDistanceHeuristic heur;
+		//heur.SetGoal(goal);
+
+
+		MySFBDS<SlidingTilePuzzleState, SlidingTilePuzzleAction, SlidingTilePuzzle, ManhattanDistanceHeuristic>
+			*sfbds;
+
+		clock_t startTime;
+		clock_t endTime;
+		clock_t clockTicksTaken;
+		double timeInSeconds;
+		//int i = atoi(argv[1]);
+		for (int i = low - 1; i < high; i++)
+		{
+			startTime = clock();
+			GetSildingTileInstance(i, start);
+			sfbds = new MySFBDS<SlidingTilePuzzleState, SlidingTilePuzzleAction, SlidingTilePuzzle, ManhattanDistanceHeuristic>(heur,heur,BF, 500.0);
+			std::cout << "********************************\n"
+				<< "Puzzle " << i + 1 << " of 100\n"
+				<< "start: " << start
+				<< "goal: " << goal << "\n";
+			std::vector<SlidingTilePuzzleAction> acs;
+			puzzle = SlidingTilePuzzle(4, 4);
+			if (sfbds->GetPath(puzzle, start, goal))
+			{
+				std::cout << "SFBDS with BF Jumping Policy found a path!\n";
+				acs = sfbds->GetActionSequence();
+				std::cout << "Path length: " << acs.size() << "\n";
+				if (showPath)
+				{
+					std::cout << "Path: ";
+					for (int i = 0; i < acs.size(); i++)
+						std::cout << acs[i] << " ";
+					std::cout << "\n";
+				}
+			}
+
+			else
+				std::cout << "SFBDS with BF Jumping Policy did not find a path.\n";
+			std::cout << "nodes expanded: " << sfbds->GetNodesExpanded() << "\n";
+			std::cout << "number of jumps: " << sfbds->GetNumOfJumps() << "\n";
+			delete sfbds;
+
+			endTime = clock();
+			clockTicksTaken = endTime - startTime;
+			timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;
+			std::cout << "time spent: " << timeInSeconds << " seconds\n";
+		}
+
+	}
+
+
+	else if (argc > 1 && strcmp(argv[1], "-oct") == 0)
+	{
+
+		Map2D map;
+		map.LoadMap("../../resources/maps/lak303d.map");
+
+		OctileDistanceHeuristic odh;
+
+
+		//load the benchmark
+		std::vector<int> startx, starty, goalx, goaly;
+		std::vector<double> expectedCost;
+		if (!LoadBenchmark(startx, starty, goalx, goaly, expectedCost, "../../resources/benchmarks/lak303d.map.scen"))
+			return 1;
+
+
+		MyAStar<Map2DState, Map2DAction, Map2D, OctileDistanceHeuristic>
+			*astar1;
+
+		clock_t startTime;
+		clock_t endTime;
+		clock_t clockTicksTaken;
+		double timeInSeconds;
+
+		double solutionCost;
+		for (int i = 0; i < startx.size(); i++)
+		{
+			Map2DState start(startx[i], starty[i]);
+			Map2DState goal(goalx[i], goaly[i]);
+
+			odh.SetGoal(goal);
+
+			astar1 = new MyAStar<Map2DState, Map2DAction, Map2D, OctileDistanceHeuristic>
+				(odh);
+
+			std::cout << "********************************\n"
+				<< "test case" << i + 1 << "\n"
+				<< "start: " << start
+				<< "goal: " << goal << "\n";
+
+			startTime = clock();
+			if (astar1->GetPath(map, start, goal))
+			{
+				std::cout << "A* w/ ODH found a path!\n";
+				std::cout << "nodes expanded:\tPath length:\ttime spent(s)\n";
+				std::cout << astar1->GetNodesExpanded() << "\t";
+				solutionCost = astar1->GetSolutionCost();
+				std::cout << solutionCost << "\t";
+			}
+
+			delete astar1;
+
+			endTime = clock();
+			clockTicksTaken = endTime - startTime;
+			timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;
+			std::cout << timeInSeconds << "\n";
+
+			if (solutionCost - expectedCost[i] > 0.01 || solutionCost - expectedCost[i] < -0.01)
+			{
+				std::cout << "error solution cost:\t expected cost\n";
+				std::cout << solutionCost << "\t" << expectedCost[i] << "\n";
+				return 1;
+			}
+			else
+				std::cout << "passed!\n";
+		}
+	}
+
+
+	else if (argc > 1 && strcmp(argv[1], "-far") == 0)
+	{
+		int k = 10;
+		if (argc > 2)
+			k = atoi(argv[2]);
+		Map2D map;
+
+		//initialize differential heuristic
+		Map2DDifferentialHeuristic diffHeur(map);
+		if (!diffHeur.LoadMap("../../resources/maps/lak303d.map"))
+		{
+			std::cout << "fail to load map: " << "../../resources/maps/lak303d.map" << "\n";
+			return 1;
+		}
+
+		//choose a random pivot and get its farthest point
+		Map2DState s;
+		std::vector<Map2DAction> actions;
+		int x, y;
+		srand(time(NULL));
+		map.LoadMap("../../resources/maps/lak303d.map");
+		while (actions.size() == 0)
+		{
+			x = rand() % map.width;
+			y = rand() % map.height;
+			s = Map2DState(x, y);
+			map.GetActions(s, actions);
+		}
+		diffHeur.AddPivot(s);
+
+		Map2DState next;
+		next = diffHeur.GetFarthestPivot();
+		diffHeur.ChangePivot(next,0);
+		//add k-1 more pivots that are farthest from current pivots
+		for (int i = 0; i < k-1; i++)
+		{
+			next = diffHeur.GetFarthestPivot();
+			diffHeur.AddPivot(next);
+		}
+		//build the PDB heuristic and save the visualization images 
+		diffHeur.BuildPDBs();
+		for (int i = -1; i < k; i++)
+			diffHeur.SaveAsBMP("../../resources/maps/lak303d_far/", i);
+
+		OctileDistanceHeuristic odh;
+
+
+		//load the benchmark
+		std::vector<int> startx, starty, goalx, goaly;
+		std::vector<double> expectedCost;
+		if (!LoadBenchmark(startx, starty, goalx, goaly, expectedCost, "../../resources/benchmarks/lak303d.map.scen"))
+			return 1;
+
+
+		MyIDAStar<Map2DState, Map2DAction, Map2D, MaxHeuristic<Map2DState>>
+			*ida;
+
+		clock_t startTime;
+		clock_t endTime;
+		clock_t clockTicksTaken;
+		double timeInSeconds;
+
+		double solutionCost;
+		for (int i = 0; i < startx.size(); i++)
+		{
+			Map2DState start(startx[i], starty[i]);
+			Map2DState goal(goalx[i], goaly[i]);
+
+			diffHeur.SetGoal(goal);
+			odh.SetGoal(goal);
+			MaxHeuristic<Map2DState> heur;
+			heur.heurs.push_back(&diffHeur);
+			heur.heurs.push_back(&odh);
+
+			ida = new MyIDAStar<Map2DState, Map2DAction, Map2D, MaxHeuristic<Map2DState>>
+				(heur,100000);
+
+			std::cout << "********************************\n"
+				<< "test case" << i + 1 << "\n"
+				<< "start: " << start
+				<< "goal: " << goal << "\n";
+
+			startTime = clock();
+			if (ida->GetPath(map, start, goal))
+			{
+				std::cout << "A* w/ Farthest Diff Heur & ODH found a path!\n";
+				std::cout << "nodes expanded:\tPath length:\ttime spent(s)\n";
+				std::cout << ida->GetNodesExpanded() << "\t";
+				solutionCost = ida->GetSolutionCost();
+				std::cout << solutionCost << "\t";
+			}
+
+			delete ida;
+
+			endTime = clock();
+			clockTicksTaken = endTime - startTime;
+			timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;
+			std::cout << timeInSeconds << "\n";
+
+			if (solutionCost - expectedCost[i] > 0.01 || solutionCost - expectedCost[i] < -0.01)
+			{
+				std::cout << "error solution cost:\t expected cost\n";
+				std::cout << solutionCost << "\t" << expectedCost[i] << "\n";
+				return 1;
+			}
+			else
+				std::cout << "passed!\n";
+		}
+	}
+
+	else if (argc > 1 && strcmp(argv[1], "-ran") == 0)
+	{
+		int k = 10;
+		if (argc > 2)
+			k = atoi(argv[2]);
+		Map2D map;
+
+		//initialize differential heuristic
+		Map2DDifferentialHeuristic diffHeur(map);
+		if (!diffHeur.LoadMap("../../resources/maps/lak303d.map"))
+		{
+			std::cout << "fail to load map: " << "../../resources/maps/lak303d.map" << "\n";
+			return 1;
+		}
+
+		//choose 10 random pivot
+		Map2DState s;
+		std::vector<Map2DAction> actions;
+		int x, y;
+		srand(time(NULL));
+		map.LoadMap("../../resources/maps/lak303d.map");
+
+		//add k pivots that are randomly chosen
+		for (int i = 0; i < k; i++)
+		{
+			actions.resize(0);
+			while (actions.size() == 0)
+			{
+				x = rand() % map.width;
+				y = rand() % map.height;
+				s = Map2DState(x, y);
+				map.GetActions(s, actions);
+			}
+			diffHeur.AddPivot(s);
+		}
+		//build the PDB heuristic and save the visualization images 
+		diffHeur.BuildPDBs();
+		for (int i = -1; i < k; i++)
+			diffHeur.SaveAsBMP("../../resources/maps/lak303d_ran/", i);
+
+		OctileDistanceHeuristic odh;
+
+
+		//load the benchmark
+		std::vector<int> startx, starty, goalx, goaly;
+		std::vector<double> expectedCost;
+		if (!LoadBenchmark(startx, starty, goalx, goaly, expectedCost, "../../resources/benchmarks/lak303d.map.scen"))
+			return 1;
+
+
+		MyAStar<Map2DState, Map2DAction, Map2D, MaxHeuristic<Map2DState>>
+			*astar1;
+
+		clock_t startTime;
+		clock_t endTime;
+		clock_t clockTicksTaken;
+		double timeInSeconds;
+
+		double solutionCost;
+		for (int i = 0; i < startx.size(); i++)
+		{
+			Map2DState start(startx[i], starty[i]);
+			Map2DState goal(goalx[i], goaly[i]);
+
+			diffHeur.SetGoal(goal);
+			odh.SetGoal(goal);
+			MaxHeuristic<Map2DState> heur;
+			heur.heurs.push_back(&diffHeur);
+			heur.heurs.push_back(&odh);
+
+			astar1 = new MyAStar<Map2DState, Map2DAction, Map2D, MaxHeuristic<Map2DState>>
+				(heur);
+
+			std::cout << "********************************\n"
+				<< "test case" << i + 1 << "\n"
+				<< "start: " << start
+				<< "goal: " << goal << "\n";
+
+			startTime = clock();
+			if (astar1->GetPath(map, start, goal))
+			{
+				std::cout << "A* w/ Random Diff Heur & ODH found a path!\n";
+				std::cout << "nodes expanded:\tPath length:\ttime spent(s)\n";
+				std::cout << astar1->GetNodesExpanded() << "\t";
+				solutionCost = astar1->GetSolutionCost();
+				std::cout << solutionCost << "\t";
+			}
+
+			delete astar1;
+
+			endTime = clock();
+			clockTicksTaken = endTime - startTime;
+			timeInSeconds = clockTicksTaken / (double)CLOCKS_PER_SEC;
+			std::cout << timeInSeconds << "\n";
+
+			if (solutionCost - expectedCost[i] > 0.01 || solutionCost - expectedCost[i] < -0.01)
+			{
+				std::cout << "error solution cost:\t expected cost\n";
+				std::cout << solutionCost << "\t" << expectedCost[i] << "\n";
+				return 1;
+			}
+			else
+				std::cout << "passed!\n";
+		}
+	}
+
+
+	else
+	{
+		std::cout << "Usage: " << argv[0] << " -oct \n"
+			<< argv[0] << " -far [k] \n"
+			<< argv[0] << " -ran [k] \n";
+	}
+
+
+	return 0;
+}
+
+
+bool LoadBenchmark(std::vector<int>& startx, std::vector<int>& starty, std::vector<int>& goalx, std::vector<int>& goaly,
+	std::vector<double>& expectedCost, std::string fileName)
+{
+	std::ifstream fin;
+	fin.open(fileName);
+	if (!fin.is_open())
+	{
+		std::cout << "fail to load benchmark file: " << fileName << "\n";
+		return false;
+	}
+
+	startx.resize(0);
+	starty.resize(0);
+	goalx.resize(0);
+	goaly.resize(0);
+	expectedCost.resize(0);
+	std::string str;
+	//get rid of "version 1"
+	fin >> str;
+	fin >> str;
+	while (!fin.eof())
+	{
+		//first 4 should be "group", "mapname", "map width", "map height"
+		//which are not helpful for this assignment
+		fin >> str;
+		fin >> str;
+		fin >> str;
+		fin >> str;
+
+		if (fin.eof())
+			break;
+
+		fin >> str;
+		startx.push_back(std::stoi(str));
+		fin >> str;
+		starty.push_back(std::stoi(str));
+		fin >> str;
+		goalx.push_back(std::stoi(str));
+		fin >> str;
+		goaly.push_back(std::stoi(str));
+		fin >> str;
+		expectedCost.push_back(std::stod(str));
+	}
+	return true;
+}
